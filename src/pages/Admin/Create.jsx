6a5cb4924918af60fe9bff996bf0daf915c2b183
Kolpaxidis V. (laptop) <kolpaxidis86@gmail.com>
2024-08-16 10:38:12 +0300

    - <hr/> fix done - Add inputs for Create.jsx - Add universal func for input state

--- src/pages/Admin/Create.jsx
+++ src/pages/Admin/Create.jsx
@@ -3,32 +3,129 @@ import Input from '../../components/input/input';
 import styles from './Create.module.scss';
 import { CREATE_CALENDAR } from '../../utils/translations'; // import translations file
 
-const page_header = CREATE_CALENDAR.CREATE_CALENDAR;
-const calendar_name = CREATE_CALENDAR.NAME.LABEL;
-const calendar_placeholder = CREATE_CALENDAR.NAME.PLACEHOLDER;
+
+
 
 
 function Create() {
-	const [calendarName, setCalendarName] = useState('');
-	const handlerCalendarName = (e) => {
-		setCalendarName(e.target.value);
-	}
+
+	const [formData, setFormData] = useState({
+		calendarName: '',
+		startDate: '',
+		endDate: '',
+		slotDuration: '0',
+		startSlotTime: '0',
+		endSlotTime: '0',
+		breakDuration: '0',
+	});
+
+	const handleInputChange = (e) => {
+		const { id, value } = e.target;
+		setFormData(prevData => ({
+			...prevData,
+			[id]: value
+		}));
+	};
 
 	return (
 		<div>
-			<h1>{page_header}</h1>
-			<Input
-				className={styles.inputs}
-				label={calendar_name}
-				id="create_calendar_name"
-				placeholder={calendar_placeholder}
-				type='text'
-				value={calendarName}
-				onChange={handlerCalendarName}
-				required
-			/>
+			<div className="calendar_title">
 
-			<hr className='h-row' /> {/* NOT VISIBLE NEED FIX */}
+				<h1>{CREATE_CALENDAR.CREATE_CALENDAR}</h1>
+				<Input
+					className={styles.inputs}
+					label={CREATE_CALENDAR.NAME.LABEL}
+					id="calendarName"
+					placeholder={CREATE_CALENDAR.NAME.PLACEHOLDER}
+					type='text'
+					value={formData.calendarName}
+					onChange={handleInputChange}
+					required
+				/>
+
+			</div>
+
+			<hr className='h-row' />
+
+			<div className="calendar_duration">
+
+				<h2>{CREATE_CALENDAR.DAYS_AND_SLOTS.TITLE}</h2>
+
+				<div className={styles.durationContainer}>
+					<Input
+						className={styles.inputs}
+						label={CREATE_CALENDAR.DAYS_AND_SLOTS.START_DATE_TITLE}
+						id="startDate"
+						type='date'
+						value={formData.startDate}
+						onChange={handleInputChange}
+						required
+					/>
+					<Input
+						className={styles.inputs}
+						label={CREATE_CALENDAR.DAYS_AND_SLOTS.END_DATE_TITLE}
+						id="endDate"
+						type='date'
+						value={formData.endDate}
+						onChange={handleInputChange}
+						required
+					/>
+				</div>
+
+				<div className={styles.durationContainer}>
+					<Input
+						className={styles.inputs}
+						label={CREATE_CALENDAR.DAYS_AND_SLOTS.SLOT_DURATION}
+						id="slotDuration"
+						type='number'
+						min="0"
+						max="60"
+						value={formData.slotDuration}
+						onChange={handleInputChange}
+						required
+					/>
+
+					<Input
+						className={styles.inputs}
+						label={CREATE_CALENDAR.DAYS_AND_SLOTS.START_SLOT}
+						id="startSlotTime"
+						type='number'
+						min="0"
+						max="60"
+						value={formData.startSlotTime}
+						onChange={handleInputChange}
+						required
+					/>
+
+					<Input
+						className={styles.inputs}
+						label={CREATE_CALENDAR.DAYS_AND_SLOTS.END_SLOT}
+						id="endSlotTime"
+						type='number'
+						min="0"
+						max="60"
+						value={formData.endSlotTime}
+						onChange={handleInputChange}
+						required
+					/>
+
+					<Input
+						className={styles.inputs}
+						label={CREATE_CALENDAR.DAYS_AND_SLOTS.BREAK_DURATION}
+						id="breakDuration"
+						type='number'
+						min="0"
+						max="60"
+						value={formData.breakDuration}
+						onChange={handleInputChange}
+						required
+					/>
+				</div>
+
+			</div>
+
+			<hr className='h-row' />
+
 
 		</div>
 	);
